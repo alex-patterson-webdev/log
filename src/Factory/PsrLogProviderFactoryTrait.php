fce7b4a746f37ca225ec23c65ba61badcd9f85f3
Alex Patterson <alex.patterson.webdev@gmail.com>
2020-08-09 12:14:13 +0000

    Update PsrLogProviderFactoryTrait to use the default logger factory, PsrLoggerFactory

--- src/Factory/PsrLogProviderFactoryTrait.php
+++ src/Factory/PsrLogProviderFactoryTrait.php
@@ -4,10 +4,6 @@ declare(strict_types=1);
 
 namespace Arp\Log\Factory;
 
-use Arp\Factory\Exception\FactoryException;
-use Arp\Factory\FactoryInterface;
-use Psr\Log\LoggerInterface;
-
 /**
  * @author  Alex Patterson <alex.patterson.webdev@gmail.com>
  * @package Arp\Log\Factory
@@ -19,5 +15,5 @@ trait PsrLogProviderFactoryTrait
     /**
      * @var string
      */
-    protected $defaultLoggerFactory = NullLoggerFactory::class;
+    protected $defaultLoggerFactory = PsrLoggerFactory::class;
 }
